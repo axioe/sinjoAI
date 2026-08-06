246fd079b17dda75ace4496dd609515d0c83517a
omn0923-wq <omn0923@gmail.com>
2026-08-06 14:53:30 +0900

    초기 파일

---
+++ back_end/src/main/java/com/slangs/sinjo/SinjoApplication.java
@@ -0,0 +1,13 @@
+package com.slangs.sinjo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinjoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SinjoApplication.class, args);
+	}
+
+}
---
+++ back_end/src/test/java/com/slangs/sinjo/SinjoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slangs.sinjo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SinjoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
